b8d78f6fc3e8eb96d1be5d184350f9b904953ad0
Vaishnavi G V S <vaishgvs@gmail.com>
2021-06-05 12:16:39 +0530

    updating sum return

---
+++ netlify/functions/sum.js
@@ -0,0 +1,15 @@
+exports.handler = async (event, context) => {
+    const num1 = event.body.First_Number;
+    const num2 = event.body.Second_Number;
+  
+    const sum = parseInt(num1) + parseInt(num2);
+    var sum2 = JSON.stringify(sum);
+    return {
+        statusCode: 200,
+        body: sum2,
+    };
+    /*return {
+      statusCode: 200,
+      body: `Sum is: ` + sum2 ,
+    };*/
+  };
