b059f47253e172eb1a006b11a49e9432e78ff56c
Vaishnavi G V S <vaishgvs@gmail.com>
2021-06-06 01:31:58 +0530

    changed to event.body.param

--- netlify/functions/sum.js
+++ netlify/functions/sum.js
@@ -3,8 +3,8 @@ exports.handler = async (event, context) => {
     //const num2 = event.body.Second_Number;
     //const body = JSON.parse(event.body).payload;
 
-    const num1 = event.queryStringParameters.First_Number;
-    const num2 = event.queryStringParameters.Second_Number;
+    const num1 = event.body.First_Number;
+    const num2 = event.body.Second_Number;
     const sum = parseInt(num1) + parseInt(num2);
     var sum2 = JSON.stringify(sum);
 
@@ -14,6 +14,6 @@ exports.handler = async (event, context) => {
     };*/
     return {
       statusCode: 200,
-      //body: `Sum is: ` + sum2 ,
+      body: `Sum is: ` + sum2 ,
     };
   };
